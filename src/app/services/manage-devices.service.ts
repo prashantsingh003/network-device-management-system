ef5401c6a913f7b226ee5125142cd2212b72e5ec
Vagaram-inuvest <support@inuvest.tech>
2023-09-29 20:05:33 +0530

    messages update

--- src/app/services/manage-devices.service.ts
+++ src/app/services/manage-devices.service.ts
@@ -15,18 +15,31 @@ export class ManageDevicesService {
   }
   addDevice(device:Device){
     this.devices.push(device);
+    this.msgService.showMessage({text:'Device Added!!',type:'primary'});
   }
   updateDevice(index:number,device:Device){
     this.devices[index]=device;
+    this.msgService.showMessage({
+      text:`Device: ${this.devices[index].name.toUpperCase()} updated!!`,
+      type:'primary'
+    });
   }
   deleteDevice(index){
-    this.devices.splice(index,1);
+    const dev=this.devices.splice(index,1)[0];
+    this.msgService.showMessage({text:'Device: '+dev.name.toUpperCase()+' deleted successfully!!',type:'warn'});
   }
   toggleDeviceStatus(index:number){
     const device=this.devices[index];
     device.active=!device.active;
+    this.msgService.showMessage({
+      text:device.name.toUpperCase()+`'s status set to ${device.active?'active':'inactive'}`,
+      type:'warn'
+    });
   }
   getDevice(index:number){
     return this.devices[index];
+  }
+  passMessage(msg){
+    this.msgService.showMessage({text:msg,type:''})
   }
 }
